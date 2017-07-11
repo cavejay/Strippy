28e23ee9c376fa7e3200e89b04670ce9de77d240
cavejay <michael.ball@dynatrace.com>
2017-07-11 16:30:13 +1000

    work on writing the testing apparatus

--- test.ps1
+++ test.ps1
@@ -15,3 +15,16 @@
 .LINK
     https://github.com/cavejay/Strippy
 #>
+
+# 2 Check there's a tests dir
+
+# 2 Make a tests/testoutput dir
+
+# 1 run a *.test file (piping it's output to tests/testoutput/std.*.out) and compare their output in tests/testoutput with the *.out files that match what was created.
+powershell .\test1.test > .\tests\testoutput\std.test1.out
+
+# 1 Record as an error if there's a difference and a pass if there is none.
+
+# 2 repeat for all .test files
+
+# 2 optionally delete the tests dir
