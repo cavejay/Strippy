9457e975a498d218ff351a1daa581d8602885ced
Michael <michael@sablecliff.com>
2017-08-22 20:12:56 +1000

    removed a bunch of test files I didn't need.

--- tests/logfile1.ignore/logfile1.test.ps1
+++
@@ -1,5 +0,0 @@
-<#
-    This is the first test
-#>
-Set-Location $input.strippy_path
-.\strippy.ps1 -config .\tests\test1\testConfig.test1.json -file .\tests\test1\test.log.test1.in -out .\tests\test1\output\test.sanitised.log.test1.out -keyout .\tests\test1output\KeyList.txt.test1.out
