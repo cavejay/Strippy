0e4dfc92e64cb0ce0266e18fd79db581ef57fef2
cavejay <michael.ball@dynatrace.com>
2017-09-07 15:25:55 +1000

    removed files

--- tests/Base.IPs/IPs.test.ps1
+++
@@ -1,5 +0,0 @@
-<#
-    This is the first test
-#>
-Set-Location $input.strippy_path
-.\strippy.ps1 -config .\tests\Base.IPs\config.json -file .\tests\Base.IPs\file.in -out .\tests\test1\output\test.sanitised.log.test1.out -keyout .\tests\test1output\KeyList.txt.test1.out
--- tests/Base.Norules/norules.test.ps1
+++
@@ -1,7 +0,0 @@
-<#
-    Test that when there are no rules there is no sanitisation
-#>
-
-Set-Location $input.strippy_path
-
-.\strippy -file .\tests\file.in -out 
