da9394b5fa2138a04a85e295b5d1301f643deb8b
cavejay <michael.ball@dynatrace.com>
2017-10-09 15:37:48 +1100

    updated replace function to match master

--- strippy.ps1
+++ strippy.ps1
@@ -499,7 +499,7 @@ $JobFunctions = {
         foreach ( $key in $( $finalKeyList.GetEnumerator() | Sort-Object { $_.Value.Length } -Descending )) {
             Write-Debug "   Substituting $($key.value) -> $($key.key)"
             Write-Progress -Activity "Sanitising $filename" -Status "Removing $($key.value)" -Completed -PercentComplete (($count++/$finalKeyList.count)*100)
-            $content = $content -replace [regex]::Escape($key.value), $key.key
+            $content = $content.Replace($key.value, $key.key)
         }
         Write-Progress -Activity "Sanitising $filename" -Completed -PercentComplete 100
     
